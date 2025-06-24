77998368bf941ba4cc2d94156c51cc998a081957
Md Sajid <mdsajid7908@gmail.com>
2025-06-24 15:15:12 +0530

    first

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -4,7 +4,7 @@ import logo from './logo2.png'
 import hero_image from './hero_image.jpg'
 import about_image from './about_image.jpg'
 import bg_video from './bg_video.mp4'
-import principal from './principal.jpg'
+import principal from './Principal.jpg'
 export const assets = {
     cross_icon,
     menu_icon,
